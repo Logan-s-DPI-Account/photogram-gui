28c1a36154b070a51578a4e35ae658f1bb1bcec4
Logan CoBell <lcobell@cps.edu>
2022-10-20 18:55:38 +0000

    user details looks good

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,23 @@ class UsersController < ApplicationController
 
     render({ :template => "user_templates/index.html.erb"})
   end
+  
+  def show
+    #Parameters: {"path_username"=>"anisa"}
+    url_username = params.fetch("path_username")
 
+    matching_usernames = User.where({ :username => url_username })
+
+    @the_user = matching_usernames.at(0)
+    #we don't have to do these for this assignment but a redirect if the value is nil woul
+    # if the_user == nil
+    #   redirect_to("/404")
+    # else
+    
+    render({ :template => "user_templates/show.html.erb"})
+
+    # end
+  end
   
 
 end
